1452721ea2ed5617f75b8440e930c56ff5e6152b
Raghav <dheeraj62844@gmail.com>
2026-04-03 19:10:49 +0530

    fix api key

--- api/generate.js
+++ api/generate.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.VITE_API_KEY}`,
+          Authorization: `Bearer ${process.env.HF_API_KEY}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
